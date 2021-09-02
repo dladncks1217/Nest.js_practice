9fa96560966fc5dcc483e00c08e317a5b7a763d9
dladncks1217 <dlaxodud1217@gmail.com>
2021-09-02 18:43:26 +0900

    api layout

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,14 @@ import { UsersModule } from './users/users.module';
 import { WorkspaceModule } from './workspace/workspace.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import { WorkspacesService } from './workspaces/workspaces.service';
+import { WorkspacesController } from './workspaces/workspaces.controller';
+import { WorkspacesModule } from './workspaces/workspaces.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, WorkspaceModule, ChannelsModule, DmsModule], // forRoot같은거 있는거는 설정할수있는 그런것들
-  controllers: [AppController],
-  providers: [AppService], // 이 provider에 연결되어있는 것을 확인한 뒤 의존성 주입을 해준다.
+  imports: [ConfigModule.forRoot(), UsersModule, WorkspaceModule, ChannelsModule, DmsModule, WorkspacesModule], // forRoot같은거 있는거는 설정할수있는 그런것들
+  controllers: [AppController, WorkspacesController],
+  providers: [AppService, WorkspacesService], // 이 provider에 연결되어있는 것을 확인한 뒤 의존성 주입을 해준다.
 })
 export class AppModule implements NestModule{
   configure(consumer: MiddlewareConsumer):any{
---
+++ src/channels/channels.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Query, Param, Body } from '@nestjs/common';
+
+@Controller('channels')
+export class ChannelsController {
+    @Get()
+    getAllChannels(){
+
+    }
+    @Post()
+    createChannel(){
+
+    }
+
+    @Get()
+    getSpecificChannels(){
+
+    }
+
+    @Get('name/chats')
+    getChats(@Query() query, @Param() param){
+        
+        console.log(query.perPage, query.page);
+        console.log(param.id, param.url)
+    }
+
+    @Post('name/chats')
+    postChat(@Body() body){}
+
+    @Get(':name/member')
+    getAllMembers(){
+
+    }
+    
+    @Post(':name/member')
+    inviteMembers(){
+        
+    }
+}
