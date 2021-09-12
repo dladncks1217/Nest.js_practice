74e56def332e33c7f565be8fb6dd84744b87a5d1
dladncks1217 <dlaxodud1217@gmail.com>
2021-09-13 02:01:53 +0900

    Add: Exception Filters

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm'; // entity에 query날리는건 repository임.
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
@@ -14,22 +14,22 @@ export class UsersService {
     async join(email:string, nickname:string, password:string){
         if(!email){
             // 이메일 없음 오류
-            throw new Error('이메일이 없습니다.');
+            throw new HttpException('이메일이 없습니다.', 400); // async함수 내에서는 throw해도 서버가 종료되지 않음.
             return;
         }
         if(!nickname){
             // 닉네임 없음 오류
-            throw new Error('닉네임이 없습니다.');
+            throw new HttpException('닉네임이 없습니다.', 400);
             return;
         }
         if(password){
             // 비밀번호 없음 오류
-            throw new Error('비밀번호가 없습니다.');
+            throw new HttpException('비밀번호가 없습니다.', 400);
             return;
         }
         const user = await this.usersRepository.findOne({where:{email}});
         if(user){
-            throw new Error('이미 존재하는 사용자입니다.')
+            throw new HttpException('이미 존재하는 사용자입니다.', 400)
             return;
         }
         const hashedPassword = await bcrypt.hash(password,12);
